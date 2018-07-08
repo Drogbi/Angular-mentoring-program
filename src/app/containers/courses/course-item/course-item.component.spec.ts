90da751ee28b61422790b1d9bac095a3378f64f0
Aliaksandr Ihnashevich <al.ignashevich@gmail.com>
2018-07-09 01:02:50 +0300

    implement some tests

--- src/app/containers/courses/course-item/course-item.component.spec.ts
+++ src/app/containers/courses/course-item/course-item.component.spec.ts
@@ -4,21 +4,33 @@ import { CourseItemComponent } from './course-item.component';
 import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { Course } from '@containers/courses/shared/course.model';
+import { HighlightBorderDirective } from '@containers/courses/course-item/highlight-border.directive';
+import { DatePipe } from '@angular/common';
+import { click } from '../../../testing';
 
 describe('CourseItemComponent', () => {
     let component: CourseItemComponent;
     let fixture: ComponentFixture<CourseItemComponent>;
     let courseItemDe: DebugElement;
+    let testCourse: Course;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [CourseItemComponent],
+            declarations: [CourseItemComponent, HighlightBorderDirective],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
         }).compileComponents();
     }));
 
     beforeEach(() => {
         fixture = TestBed.createComponent(CourseItemComponent);
+        testCourse = {
+            id: 12,
+            creationDate: new Date(),
+            description: 'Text',
+            durationMin: 60,
+            title: 'Awesome course',
+            topRated: true,
+        };
         component = fixture.componentInstance;
         fixture.detectChanges();
         courseItemDe = fixture.debugElement.query(By.css('.course'));
@@ -26,5 +38,39 @@ describe('CourseItemComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should display course title', () => {
+        component.course = testCourse;
+        fixture.detectChanges();
+        expect(
+            fixture.debugElement.nativeElement.querySelector('.course__title').textContent
+        ).toContain(testCourse.title);
+    });
+
+    it('should display course duration', () => {
+        component.course = testCourse;
+        fixture.detectChanges();
+        expect(
+            fixture.debugElement.nativeElement.querySelector('.course__duration').textContent
+        ).toContain(testCourse.durationMin);
+    });
+
+    it('should display course creation date', () => {
+        const datePipe = new DatePipe('en');
+        component.course = testCourse;
+        fixture.detectChanges();
+        const courseDurationEl = fixture.debugElement.nativeElement.querySelector(
+            '.course__creation-date'
+        );
+        expect(courseDurationEl.textContent).toContain(datePipe.transform(testCourse.creationDate));
+    });
+
+    it('should create top rated icon if course is top rated', () => {
+        component.course = testCourse;
+        fixture.detectChanges();
+        expect(
+            fixture.debugElement.nativeElement.querySelector('.course__top-rated-icon')
+        ).not.toBeNull();
     });
 });
--- src/app/containers/courses/courses-toolbox/courses-toolbox.component.spec.ts
+++ src/app/containers/courses/courses-toolbox/courses-toolbox.component.spec.ts
@@ -4,24 +4,23 @@ import { CoursesToolboxComponent } from './courses-toolbox.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('CoursesToolboxComponent', () => {
-  let component: CoursesToolboxComponent;
-  let fixture: ComponentFixture<CoursesToolboxComponent>;
+    let component: CoursesToolboxComponent;
+    let fixture: ComponentFixture<CoursesToolboxComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CoursesToolboxComponent ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [CoursesToolboxComponent],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CoursesToolboxComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CoursesToolboxComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
