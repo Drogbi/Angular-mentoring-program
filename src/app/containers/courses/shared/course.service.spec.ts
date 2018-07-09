c5a99d1a7a9e5816d1366a2e98a97cefed489be6
Aliaksandr_Ihnashevich <al.ignashevich@gmail.com>
2018-07-09 15:28:52 +0300

    implement course service tests

--- src/app/containers/courses/shared/course.service.spec.ts
+++ src/app/containers/courses/shared/course.service.spec.ts
@@ -3,13 +3,29 @@ import { TestBed, inject } from '@angular/core/testing';
 import { CourseService } from './course.service';
 
 describe('CourseService', () => {
+    let service: CourseService;
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [CourseService],
         });
+        service = TestBed.get(CourseService);
     });
 
-    it('should be created', inject([CourseService], (service: CourseService) => {
+    it('should be created', () => {
         expect(service).toBeTruthy();
-    }));
+    });
+
+    it('should return courses', () => {
+        service.getCourses().subscribe(courses => expect(courses.length).toBeGreaterThan(0));
+    });
+
+    it('should return deleted course', () => {
+        service.getCourses().subscribe(courses => {
+            expect(
+                service
+                    .deleteCourse(courses[0].id)
+                    .subscribe(deletedCourse => (deletedCourse.id = courses[0].id))
+            );
+        });
+    });
 });
