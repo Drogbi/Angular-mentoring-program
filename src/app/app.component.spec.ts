b9afe45d5c7b86606d721bc6ee3544b16cb45f99
Aliaksandr_Ihnashevich <al.ignashevich@gmail.com>
2018-07-17 15:31:52 +0300

    test fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FilterCourseItemsPipe } from '@containers/courses/courses-list/filter-course-items.pipe';
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [AppComponent],
+            providers: [FilterCourseItemsPipe],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
         }).compileComponents();
     }));
