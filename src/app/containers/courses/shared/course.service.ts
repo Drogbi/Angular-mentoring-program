d23e1ed328fb43b66695ea9402b9759e3a0d25c7
Aliaksandr Ihnashevich <al.ignashevich@gmail.com>
2018-07-20 23:07:39 +0300

    implement course services

--- src/app/containers/courses/shared/course.service.ts
+++ src/app/containers/courses/shared/course.service.ts
@@ -3,7 +3,7 @@ import { Course } from '@containers/courses/shared/course.model';
 import { Observable, from, of } from 'rxjs';
 import { TransitiveCompileNgModuleMetadata } from '@angular/compiler';
 
-const courses = [
+let courses = [
     {
         id: 0,
         title: 'JavaScript Functions Decorators Generators',
@@ -70,5 +70,28 @@ export class CourseService {
 
     deleteCourse(id: number): Observable<Course> {
         return of<Course>(courses.splice(courses.findIndex(item => item.id === id), 1).pop());
+    }
+
+    createCourse(course: Course): Observable<boolean> {
+        courses = courses.concat(course);
+        if (courses.find(value => value.id === course.id)) {
+            return of<boolean>(true);
+        } else {
+            return of<boolean>(false);
+        }
+    }
+
+    updateCourse(courseId: number, newCourse: Course): Observable<boolean> {
+        const course = courses.find(value => value.id === courseId);
+        courses.splice(courses.findIndex(item => item.id === course.id), 1, newCourse);
+        if (courses.find(value => value.id === newCourse.id)) {
+            return of<boolean>(true);
+        } else {
+            return of<boolean>(false);
+        }
+    }
+
+    getCourseById(id: number) {
+        return of<Course>(courses.find(value => value.id === id));
     }
 }
