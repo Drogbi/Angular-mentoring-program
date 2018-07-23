a447b77ff2c2b14ad26d0c760789a3dd4caa8e50
Aliaksandr Ihnashevich <al.ignashevich@gmail.com>
2018-07-24 00:28:05 +0300

    fix log out button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../containers/auth/shared/auth.service';
-import { Router } from '../../../../node_modules/@angular/router';
+import { Router, ActivationEnd } from '@angular/router';
 
 @Component({
     selector: 'app-header',
@@ -10,7 +10,17 @@ import { Router } from '../../../../node_modules/@angular/router';
 export class HeaderComponent implements OnInit {
     constructor(public authService: AuthService, private router: Router) {}
 
-    ngOnInit() {}
+    public isAuthenticated: boolean;
+
+    ngOnInit() {
+        this.router.events.subscribe(event => {
+            if (event instanceof ActivationEnd) {
+                this.authService.isAuthenticated().subscribe(value => {
+                    this.isAuthenticated = value;
+                });
+            }
+        });
+    }
 
     public onLogoutClick(): void {
         this.authService.logout();
